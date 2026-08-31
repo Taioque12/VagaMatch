19dcf66fed1ddfd84deafdbea8707611ef3a7e39
Taioque12 <78626106+Taioque12@users.noreply.github.com>
2026-08-31 08:48:47 -0300

    feat(ugc): serve showcase by default and live app on demand

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { UgcAppV2 } from "./ugc/UgcAppV2.jsx";
+import { UgcShowcase } from "./ugc/UgcShowcase.jsx";
+import "./ugc/ugc-showcase.css";
 
 export function App() {
-  return <UgcAppV2 />;
+  const live = new URLSearchParams(window.location.search).get("live") === "1";
+  return live ? <UgcAppV2 /> : <UgcShowcase />;
 }
